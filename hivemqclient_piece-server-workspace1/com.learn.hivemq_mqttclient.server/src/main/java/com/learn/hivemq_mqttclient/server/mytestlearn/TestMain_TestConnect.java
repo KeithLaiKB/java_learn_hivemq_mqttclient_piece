144bf19e585de5551768ccd38a16312c5305618d
KeithLaiKB <laidelivery@163.com>
2021-11-18 00:35:34 +0800

    update something

--- hivemqclient_piece-server-workspace1/com.learn.hivemq_mqttclient.server/src/main/java/com/learn/hivemq_mqttclient/server/mytestlearn/TestMain_TestConnect.java
+++ hivemqclient_piece-server-workspace1/com.learn.hivemq_mqttclient.server/src/main/java/com/learn/hivemq_mqttclient/server/mytestlearn/TestMain_TestConnect.java
@@ -1,4 +1,4 @@
-package com.learn.hivemq_mqttclient.server;
+package com.learn.hivemq_mqttclient.server.mytestlearn;
 
 import java.net.InetSocketAddress;
 import java.util.UUID;
@@ -13,6 +13,7 @@ import com.hivemq.client.mqtt.datatypes.MqttQos;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5AsyncClient;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5Client;
+import com.hivemq.client.mqtt.mqtt5.message.connect.connack.Mqtt5ConnAck;
 import com.hivemq.client.mqtt.mqtt5.message.publish.Mqtt5Publish;
 import com.hivemq.client.mqtt.mqtt5.message.publish.Mqtt5PublishBuilder;
 import com.hivemq.client.mqtt.mqtt5.message.publish.Mqtt5PublishBuilderBase;
@@ -22,14 +23,26 @@ import com.hivemq.client.mqtt.mqtt5.message.publish.Mqtt5PublishResult;
  * 
  * <p>
  * 							description:																			</br>	
- * &emsp;						use different value to publish message each time 									</br>	
+ * &emsp;						test1 send 1 message without waiting for the successful connection					</br>
+ * &emsp;						test2 send 1 message after successful connection									</br>
+ * &emsp;						test whether we should wait for the successful connection before publishing message	</br>	
  * 																													</br>
  *
- *
+ * 
+ * 测试出test1 这种不等待 就直接publish的 方式 是容易接受不到这一条内容的
+ * 测试出test2 这种等待     是相对正确的做法
+ * 
+ * 在这个案例就可以发现 如果只是发送一条 test1 就已经容易丢失信息了
+ * 所以
+ * 在发送很多 message的 例子中, 
+ * test1 容易发送不出去第一条, 因为没有等待 connect成功
+ * 
+ * 
+ * 
  * @author laipl
  *
  */
-public class TestMain_Test2 {
+public class TestMain_TestConnect {
 
 
 	public static void main(String[] args) {
@@ -47,35 +60,11 @@ public class TestMain_Test2 {
         //MemoryPersistence persistence = new MemoryPersistence();
 
         
-        /*
-        Mqtt5BlockingClient client = Mqtt5Client.builder()
-                .identifier(UUID.randomUUID().toString())
-                .serverHost("broker.hivemq.com")
-                .buildBlocking();
-        */
-        final InetSocketAddress LOCALHOST_EPHEMERAL1 = new InetSocketAddress("localhost",1883);
-        //  builder.serverAddress是去   MqttRxClientBuilder 的父类的     MqttRxClientBuilderBase的 serverAddress
-        //  builder.buildAsync   是去  MqttRxClientBuilder 的父类的     MqttRxClientBuilderBase的 buildAsync
-        // 其过程中会经过
-        // MqttAsyncClient(final @NotNull MqttRxClient delegate) {
-        //    this.delegate = delegate;
-        // }
-        // 所以初步认为 MqttAsyncClient 是包含了 MqttRxClient 
-        //Mqtt5AsyncClient client1 = Mqtt5Client.builder().serverAddress(LOCALHOST_EPHEMERAL1).identifier(clientId).buildAsync();
-        
-        //client1.connect();
-        
-        /*
-        Mqtt5BlockingClient client = Mqtt5Client.builder()
-                .serverHost("localhost")
-                .buildBlocking();
 
-        client.connect();
-        client.publishWith().topic("test/topic").qos(MqttQos.AT_LEAST_ONCE).payload("hello".getBytes()).send();
-        */
-        
+        final InetSocketAddress LOCALHOST_EPHEMERAL1 = new InetSocketAddress("localhost",1883);
         final Mqtt5AsyncClient client = Mqtt5Client.builder().serverHost("localhost").buildAsync();
         /*
+        // 这个只是参考写法而已 你可以删掉
         client.connect()
                 .thenAccept(connAck -> System.out.println("connected " + connAck))
                 .thenCompose(v -> client.publishWith().topic("demo/topic/b").qos(MqttQos.EXACTLY_ONCE).send())
@@ -83,9 +72,39 @@ public class TestMain_Test2 {
                 .thenCompose(v -> client.disconnect())
                 .thenAccept(v -> System.out.println("disconnected"));
 		*/
-        client.connect().thenAccept(connAck -> client.publishWith().topic("test/topic").qos(MqttQos.AT_LEAST_ONCE).payload("hello".getBytes()).send()
-        		);
         
+        
+        
+        
+        //---------------------------------- Test1 案例 ----------------------------------
+        //client.connect().thenAccept(connAck -> client.publishWith().topic("test/topic").qos(MqttQos.AT_LEAST_ONCE).payload("hello".getBytes()).send());
+        
+        
+        
+        
+        //---------------------------------- Test2 案例 ----------------------------------
+        CompletableFuture<Mqtt5ConnAck> cplfu_connect_rslt = client.connect();
+        //wait
+    	while (cplfu_connect_rslt.isDone() == false) {
+    		// 这里的 sleep 可以不用, 不影响主逻辑
+    		// 只不过 这里加了个 sleep, 可以减少 不停地loop, 因为太多loop会给计算机带来的资源消耗
+    		
+        	try {
+        		Thread.sleep(1000);
+    		} catch (InterruptedException e) {
+    			// TODO Auto-generated catch block
+    			e.printStackTrace();
+    		}
+    	}
+    	cplfu_connect_rslt.thenAccept(connAck -> client.publishWith().topic(topic).qos(MqttQos.AT_LEAST_ONCE).payload("hello".getBytes()).send());
+        
+    	
+    	
+    	
+    	
+    	
+    	
+    	
         System.out.println("see that everything above is async");
         for (int i = 0; i < 5; i++) {
         	try {
--- hivemqclient_piece-server-workspace1/com.learn.hivemq_mqttclient.server/src/main/java/com/learn/hivemq_mqttclient/server/mytestlearn/TestReactiveProgram.java
+++ hivemqclient_piece-server-workspace1/com.learn.hivemq_mqttclient.server/src/main/java/com/learn/hivemq_mqttclient/server/mytestlearn/TestReactiveProgram.java
@@ -1,12 +1,34 @@
-package com.learn.hivemq_mqttclient.server;
+package com.learn.hivemq_mqttclient.server.mytestlearn;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
+/**
+ * 
+ * @author laipl
+ *
+ * 1. 给予一个 num1,
+ * 2. 把他传入CompletableFuture 进行 +1+1的处理
+ * 3. 给一定的时间, 让CompletableFuture的那个过程完成
+ * 4. 再来查看 num1的值
+ * 
+ * 发现: 
+ * 1. num1 是没有被改变的
+ * 2. CompletableFuture的 result确实是改变的
+ * 
+ * 思考:
+ * CompletableFuture 应该 是有自己的线程池的, 并进行存放自己的局部变量
+ *
+ */
 public class TestReactiveProgram {
 	public static void main(String[] args) {
+		// 给一个要观测的变量
 		int num1=0;
 		
+		// 这个"a" 只是一个名字而已
+		// 在这个过程中 
+		// num1 = 0 	-> num1 +1 -> numTmp1
+		// -> numTmp1+1 -> numTmp2 -> result = 2
 		CompletableFuture<Integer> future1  = CompletableFuture.completedFuture("a").thenApply((s) -> { 
 			
 			Integer numTmp1 = num1+1;
@@ -21,29 +43,8 @@ public class TestReactiveProgram {
 				
 			});
 		
-		/*
-		Integer result1 = null;
-		try {
-			result1 = CompletableFuture.completedFuture("a").thenApply((s) -> { 
-				
-				Integer numTmp1 = num1+1;
-				System.out.println("hi:"+numTmp1);
-				return numTmp1;
-				}).thenApply((rs) -> { 
-					Integer numTmp2 = rs+1;
-					System.out.println("hi:"+numTmp2);
-					return numTmp2;
-				}).get();
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		} catch (ExecutionException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		*/
 		
-		
+		// 等待五秒 给足够的时间 给 completefuture完成 
     	try {
     		Thread.sleep(5000);
 		} catch (InterruptedException e) {
@@ -51,11 +52,15 @@ public class TestReactiveProgram {
 			e.printStackTrace();
 		}
 		
+    	// 此时探讨  completefuture 的过程 完成后, 看一下 num1 是否 有被改变
+    	// 发现 其实num1 没被改变
 		System.out.println(num1);
 		
-		
+		 
 		Integer result1 = null; 
 		try {
+			// 然而 completefuture 的result 是被改变的
+			// 所以我感觉completefuture 有自己的线程池, 和 用线程内的局部变量 
 			result1 = future1.get();
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
